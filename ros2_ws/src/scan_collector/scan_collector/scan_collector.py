f8db1c17ff3f7a444907aaf82ad8f1c4ed85f2dd
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2024-01-21 14:54:51 +0100

    Update scan_collector with new service handler

--- ros2_ws/src/scan_collector/scan_collector/scan_collector.py
+++ ros2_ws/src/scan_collector/scan_collector/scan_collector.py
@@ -3,6 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from .utils.entity_service_handler import EntityServiceHandler
+from .utils.physics_service_handler import PhysicsServiceHandler
 
 
 class ScanCollector(Node):
@@ -14,6 +15,7 @@ class ScanCollector(Node):
         super().__init__("scan_collector")
         self.get_logger().info("ScanCollector node started.")
         self.__entity_service_handler = EntityServiceHandler(self)
+        self.__physics_service_handler = PhysicsServiceHandler(self)
 
     def spawn_entity(self, x_set: float, y_set: float) -> None:
         """
@@ -33,13 +35,31 @@ class ScanCollector(Node):
 
         self.__entity_service_handler.delete_entity()
 
+    def pause_physics(self) -> None:
+        """
+        Pauses the physics using the physics service handler.
+        """
+
+        self.__physics_service_handler.pause_physics()
+
+    def unpause_physics(self) -> None:
+        """
+        Unpauses the physics using the physics service handler.
+        """
+
+        self.__physics_service_handler.unpause_physics()
+
 
 def main(args=None):
     rclpy.init(args=args)
     node = ScanCollector()
-    for i in range(100):
-        print(f"Spawning entity {i}")
+    for i in range(1000):
+        print(f"Iteraion {i}")
+        node.pause_physics()
         node.spawn_entity(0.0, 0.0)
+        node.unpause_physics()
+        # Collect laser data
+        node.pause_physics()
         node.delete_entity()
     node.destroy_node()
     rclpy.shutdown()
