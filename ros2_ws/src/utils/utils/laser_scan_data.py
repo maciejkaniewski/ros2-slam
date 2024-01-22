d6cd0f765e44238f22874a7c9de4b3ad0485f97a
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2024-01-22 19:06:53 +0100

    Implement class for storing laser data

---
+++ ros2_ws/src/utils/utils/laser_scan_data.py
@@ -0,0 +1,46 @@
+import array
+from dataclasses import dataclass
+from typing import Tuple
+
+import numpy as np
+
+
+@dataclass
+class LaserScanData:
+    """
+    Represents laser scan data with coordinates and measurements.
+    """
+
+    _coords: Tuple[float, float]
+    _measurements: np.ndarray
+
+    def __init__(self, coords: Tuple[float, float], measurements: array.array):
+        """
+        Initializes a LaserScanData object.
+
+        Args:
+            coords (Tuple[float, float]): The coordinates of the laser scan data.
+            measurements (array.array): The measurements of the laser scan data.
+        """
+        self._coords = coords
+        self._measurements = np.asarray(measurements, dtype=float)
+
+    @property
+    def coords(self) -> Tuple[float, float]:
+        """
+        Get the coordinates of the laser scan data.
+
+        Returns:
+            Tuple[float, float]: The coordinates of the laser scan data.
+        """
+        return self._coords
+
+    @property
+    def measurements(self) -> np.ndarray:
+        """
+        Get the measurements of the laser scan data.
+
+        Returns:
+            np.ndarray: The measurements of the laser scan data.
+        """
+        return self._measurements
