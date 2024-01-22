02fe2681b014ed492b70b248d0b6d54c783a6d5c
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2024-01-22 19:06:19 +0100

    Implement PGM map loader

---
+++ ros2_ws/src/utils/utils/map_loader.py
@@ -0,0 +1,67 @@
+import matplotlib.image as mpimg
+import numpy as np
+import yaml
+
+
+class MapLoader:
+    """
+    A class for loading map data from YAML and PGM files.
+
+    Args:
+        yaml_file_path (str): The file path to the YAML metadata file.
+        pgm_file_path (str): The file path to the PGM image file.
+
+    Attributes:
+        yaml_file_path (str): The file path to the YAML metadata file.
+        pgm_file_path (str): The file path to the PGM image file.
+        metadata (dict): The metadata loaded from the YAML file.
+        img (numpy.ndarray): The image loaded from the PGM file.
+        obstacle_coords (list): The coordinates of the obstacles in the map.
+
+    """
+
+    def __init__(self, yaml_file_path, pgm_file_path):
+        self.yaml_file_path = yaml_file_path
+        self.pgm_file_path = pgm_file_path
+        self.metadata = self.load_yaml_metadata()
+        self.img, self.obstacle_coords = self.load_map()
+
+    def load_yaml_metadata(self):
+        """
+        Load the metadata from the YAML file.
+
+        Returns:
+            dict: The metadata loaded from the YAML file.
+
+        """
+        with open(self.yaml_file_path, "r") as file:
+            metadata = yaml.safe_load(file)
+        return metadata
+
+    def load_map(self):
+        """
+        Load the map image and obstacle coordinates.
+
+        Returns:
+            tuple: A tuple containing the map image (numpy.ndarray) and the obstacle coordinates (list).
+
+        """
+        img = mpimg.imread(self.pgm_file_path)
+        resolution = self.metadata["resolution"]
+        origin = self.metadata["origin"]
+        height, width = img.shape
+        y_coords, x_coords = np.where(img == 0)
+        map_x_coords = x_coords * resolution + origin[0]
+        map_y_coords = (height - y_coords) * resolution + origin[1]
+        obstacle_coords = list(zip(map_x_coords, map_y_coords))
+        return img, obstacle_coords
+
+    def get_obstacle_coordinates(self):
+        """
+        Get the coordinates of the obstacles in the map.
+
+        Returns:
+            list: The coordinates of the obstacles in the map.
+
+        """
+        return self.obstacle_coords
